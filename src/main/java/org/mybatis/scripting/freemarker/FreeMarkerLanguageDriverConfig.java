10fe2b5a0a7ff076ae89cd0576ab108ce821fd5e
Jeremy Landis <jeremylandis@hotmail.com>
2021-05-08 18:55:24 -0400

    [ci] Update copyright dates / remove one unused import

--- src/main/java/org/mybatis/scripting/freemarker/FreeMarkerLanguageDriverConfig.java
+++ src/main/java/org/mybatis/scripting/freemarker/FreeMarkerLanguageDriverConfig.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2015-2020 the original author or authors.
+ *    Copyright 2015-2021 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -40,7 +40,6 @@ import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
 import org.apache.ibatis.reflection.wrapper.DefaultObjectWrapperFactory;
 
 import freemarker.template.Configuration;
-import freemarker.template.Version;
 
 /**
  * Configuration class for {@link FreeMarkerLanguageDriver}.
