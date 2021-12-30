9d56ffe024f63814a3390cfa967eb64267ffa82f
Iwao AVE! <harawata@gmail.com>
2021-12-30 21:34:56 +0900

    wagon-git commit

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.mybatis.scripting.freemarker"},{"l":"org.mybatis.scripting.freemarker.support"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.mybatis.scripting.freemarker"},{"l":"org.mybatis.scripting.freemarker.support"}];updateSearchResults();
