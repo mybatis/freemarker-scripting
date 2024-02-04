55e25527f4fc62b636fc0adeb76d4e0013a8ff97
hazendaz <jeremylandis@hotmail.com>
2024-02-04 20:46:08 +0000

    Deploying to gh-pages from @ mybatis/freemarker-scripting@6e48064dc1af0851eb9f38fef2ca9d4da55b7dab 🚀

--- apidocs/assets/navtree_data.js
+++
@@ -1,7 +0,0 @@
-var NAVTREE_DATA =
-[ [ "org.mybatis.scripting.freemarker", "org/mybatis/scripting/freemarker/package-summary.html", [ [ "Classes", null, [ [ "FreeMarkerLanguageDriver", "org/mybatis/scripting/freemarker/FreeMarkerLanguageDriver.html", null, "" ], [ "FreeMarkerSqlSource", "org/mybatis/scripting/freemarker/FreeMarkerSqlSource.html", null, "" ], [ "GeneratedParamsTemplateModel", "org/mybatis/scripting/freemarker/GeneratedParamsTemplateModel.html", null, "" ], [ "MyBatisParamDirective", "org/mybatis/scripting/freemarker/MyBatisParamDirective.html", null, "" ], [ "ParamObjectAdapter", "org/mybatis/scripting/freemarker/ParamObjectAdapter.html", null, "" ] ]
-, "" ] ]
-, "" ] ]
-
-;
-
--- apidocs/reference/lists.js
+++
@@ -1,9 +0,0 @@
-var DATA = [
-      { id:0, label:"org.mybatis.scripting.freemarker", link:"org/mybatis/scripting/freemarker/package-summary.html", type:"package" },
-      { id:1, label:"org.mybatis.scripting.freemarker.FreeMarkerLanguageDriver", link:"org/mybatis/scripting/freemarker/FreeMarkerLanguageDriver.html", type:"class" },
-      { id:2, label:"org.mybatis.scripting.freemarker.FreeMarkerSqlSource", link:"org/mybatis/scripting/freemarker/FreeMarkerSqlSource.html", type:"class" },
-      { id:3, label:"org.mybatis.scripting.freemarker.GeneratedParamsTemplateModel", link:"org/mybatis/scripting/freemarker/GeneratedParamsTemplateModel.html", type:"class" },
-      { id:4, label:"org.mybatis.scripting.freemarker.MyBatisParamDirective", link:"org/mybatis/scripting/freemarker/MyBatisParamDirective.html", type:"class" },
-      { id:5, label:"org.mybatis.scripting.freemarker.ParamObjectAdapter", link:"org/mybatis/scripting/freemarker/ParamObjectAdapter.html", type:"class" }
-
-    ];
