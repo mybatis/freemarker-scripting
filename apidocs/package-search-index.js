af910dd702e3ae098e55d4a5fbe4664caff089e3
Jeremy Landis <jeremylandis@hotmail.com>
2025-11-23 13:03:49 -0500

    Site checkin for project MyBatis FreeMarker

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.mybatis.scripting.freemarker"},{"l":"org.mybatis.scripting.freemarker.support"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"org.mybatis.scripting.freemarker"},{"l":"org.mybatis.scripting.freemarker.support"}];updateSearchResults();
